1bdbdb8a8ea2b14210bd99fe62206a9988f0b7ab
Mohamed Salem <mo.zaher.salem@gmail.com>
2017-07-16 15:35:16 +0400

    Sacafold ready

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,91 +1,117 @@
 module.exports = function (grunt) {
-    'use strict';
+  'use strict';
 
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        banner: '/*\n' +
-        ' * <%= pkg.name %> - v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %>\n' +
-        ' * Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>; Licensed MIT\n' +
-        ' */\n\n',
-        meta: {
-            version: '0.0.0'
-        },
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    banner: '/*\n' +
+    ' * <%= pkg.name %> - v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %>\n' +
+    ' * Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>; Licensed MIT\n' +
+    ' */\n\n',
+    meta: {
+      version: '0.0.0'
+    },
 
-        concat: {
-            options: {
-                banner: '<%= banner %>',
-                separator: '\n'
-            },
-            dist: {
-                src: ['js/src/**/*.js'],
-                dest: 'js/dist/app.js'
-            }
-        },
-        
-        jshint: {
-            files: ['Gruntfile.js', 'js/src/**/*.js', 'test/**/*.js'],
-            options: {
-                jshintrc: true
-            }
-        },
+    concat: {
+      options: {
+        banner: '<%= banner %>',
+        separator: '\n'
+      },
+      dist: {
+        src: ['js/src/**/*.js'],
+        dest: 'dist/app.js'
+      }
+    },
 
-        uglify : {
-            options: {
-                mangle: {
-                    reserved: ['jQuery', 'Backbone']
-                }
-            },
-            js: {
-                files: {
-                    'js/dist/app.min.js' : [ 'js/dist/app.js' ]
-                }
-            }
-        },
-        
-        jasmine: {
-            src: 'js/src/**/*.js',
-            options: {
-                specs: 'tests/test_*.js',
-                helpers: 'tests/helpers/helper_*.js',
-                vendor: 'vendor/**/*.js'
-            }
-        },
-        
-        less: {
-            production: {
-                options: {
-                    banner: '<%= banner %>',
-                    // paths for @import directives
-                    paths: [
-                        'css/src'
-                    ],
-                    outputSourceFiles: true
-                },
-                files: {
-                    'css/dist/global.css': 'css/src/global.less'
-                }
-            }
-        },
-        
-        cssmin : {
-            css:{
-                src: 'css/dist/global.css',
-                dest: 'css/dist/global.min.css'
-            }
-        },
+    jshint: {
+      files: ['Gruntfile.js', 'js/src/**/*.js', 'test/**/*.js'],
+      options: {
+        jshintrc: true
+      }
+    },
 
-        watch: {
-            files: ['<%= jshint.files %>'],
-            tasks: ['jshint', 'jasmine'] // templates here
+    uglify : {
+      options: {
+        mangle: {
+          reserved: ['jQuery']
         }
-    });
-    
-    
+      },
+      js: {
+        files: {
+          'dist/app.min.js' : [ 'dist/app.js' ]
+        }
+      }
+    },
+
+    jasmine: {
+      src: 'js/src/**/*.js',
+      options: {
+        specs: 'tests/test_*.js',
+        helpers: 'tests/helpers/helper_*.js',
+        vendor: 'vendor/**/*.js'
+      }
+    },
+
+    less: {
+    production: {
+      options: {
+        banner: '<%= banner %>',
+        // paths for @import directives
+        paths: [
+        'css/src'
+        ],
+        outputSourceFiles: true
+      },
+      files: {
+        'dist/global.css': 'css/src/global.less'
+      }
+    }
+  },
+
+  cssmin : {
+    css:{
+      src: 'dist/global.css',
+      dest: 'dist/global.min.css'
+    }
+  },
+
+  include_js: {
+    source: {
+      files: {
+        'index.html': [
+        'dist/app.js'
+        ]
+      }
+    },
+    build: {
+      files: {
+        'index.html': [
+        'dist/app.min.js'
+        ]
+      }
+    }
+  },
+
+  processhtml: {
+    options: {
+      // Task-specific options go here. 
+    },
+    dist: {
+      files: {
+        'dist/upload/index.html': ['index.html']
+      }
+    }
+  },
+
+  watch: {
+    files: ['<%= jshint.files %>', '*.js', '*.css'],
+    tasks: ['concat', 'less',  'jshint', 'jasmine'] // templates here
+  }
+});
+
     // basics
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-jasmine');
 
-    
     // css framework
     grunt.loadNpmTasks('grunt-contrib-less');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
@@ -96,10 +122,13 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-watch');
     
+    // html include
+    grunt.loadNpmTasks('grunt-include-js');
+    grunt.loadNpmTasks('grunt-processhtml');
 
     // tasks
     grunt.registerTask('test', ['jshint', 'jasmine']);
-    grunt.registerTask('default', ['concat', 'less']);
-    grunt.registerTask('minify', ['uglify', 'cssmin']);
+    grunt.registerTask('default', ['concat', 'less', 'include_js:source']);
+    grunt.registerTask('dist', ['uglify', 'cssmin', 'include_js:build', 'processhtml']);
 
-};
+  };
